b5f585fffe09a09683d6eb3fc031f0e8c9f9a04f
iTrong <trongtrong03@gmail.com>
2020-03-13 08:36:01 +0800

    20200313-v1

--- assets/js/vue-app.js
+++ assets/js/vue-app.js
@@ -1,6 +1,30 @@
 var wrap = new Vue({
     el: '#wrap',
     data: {
-        isActive: false,
-    }
+        h_list: [
+        	{
+        		id: 0,
+        		href: 'xxx',
+        		title: 'xxxxxx',
+        		date: '2020.02.02',
+        		tag1: true,
+        		tag1_name: '111',
+        		tag2: true,
+        		tag2_name: '222',
+        		img: true,
+        		img_name: 'img_1'
+        	},
+        ],
+
+        query: "",
+    },
+
+    computed: {
+        filterHtml: function() {
+            var search = this;
+            return this.h_list.filter(function(item) {
+                return item.title.toLowerCase().indexOf(search.query.toLowerCase()) !== -1;
+            });
+        },
+    },
 })
